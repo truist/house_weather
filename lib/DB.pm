5b774a948838027a9a8b41d5e3638b52ad901468
Nathan Arthur <narthur-git@rainskit.com>
2018-12-15 21:52:07 -0500

    Extract a DB package and actually start inserting data

---
+++ lib/DB.pm
@@ -0,0 +1,59 @@
+package DB;
+
+use strict;
+use warnings;
+use v5.10;
+
+use DBI;
+use DBD::SQLite;
+
+my $TABLE_NAME = 'data';
+
+my $ID_COL = 'ID';
+my $DATE_COL = 'datetime';
+my $SOURCE_COL = 'source';
+my $TEMP_COL = 'temp';
+my $HUMIDITY_COL = 'humidity';
+
+sub init {
+	my ($package, $config) = @_;
+
+	my $dbh = DBI->connect(
+		'DBI:SQLite:dbname=' . $config->{db_file},
+		undef, undef,
+		{ RaiseError => 1, AutoCommit => 1 }
+	) or die $DBI::errstr;
+
+	my $statement = qq|
+		CREATE TABLE IF NOT EXISTS $TABLE_NAME (
+			$ID_COL        INTEGER  PRIMARY KEY NOT NULL,
+			$DATE_COL      TEXT     NOT NULL,
+			$SOURCE_COL    TEXT     NOT NULL,
+			$TEMP_COL      REAL     NOT NULL,
+			$HUMIDITY_COL  REAL     NOT NULL
+		)
+	|;
+	$dbh->do($statement);
+
+	my $self = bless({}, $package);
+	$self->{dbh} = $dbh;
+
+	return $self;
+}
+
+sub add_record {
+	my ($self, $source, $temp, $humidity) = @_;
+
+	my $statement = qq|
+		INSERT INTO $TABLE_NAME (
+			$DATE_COL,
+			$SOURCE_COL, $TEMP_COL, $HUMIDITY_COL
+		) VALUES (
+			DATETIME('now'),
+			?, ?, ?
+		)
+	|;
+	$self->{dbh}->do($statement, undef, $source, $temp, $humidity);
+}
+
+1;
--- lib/HouseWeather/Controller/Home.pm
+++ lib/HouseWeather/Controller/Home.pm
@@ -7,4 +7,23 @@ sub welcome {
 	$self->render(msg => 'Welcome to the Mojolicious real-time web framework!');
 }
 
+sub submit {
+	my ($self) = @_;
+
+	my $source = $self->get_required_param('source');
+	my $temp = $self->get_required_param('temperature');
+	my $humidity = $self->get_required_param('humidity');
+
+	$self->db->add_record($source, $temp, $humidity);
+
+	$self->render(text => 'OK');
+}
+
+sub get_required_param {
+	my ($self, $name) = @_;
+
+	my $value = $self->param($name) or die "Argument '$name' is requred";
+	return $value;
+}
+
 1;
