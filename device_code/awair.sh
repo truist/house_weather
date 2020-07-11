972491c2c22a0a433d769f504919b8a1be57bbdf
Nathan Arthur <narthur-git@rainskit.com>
2020-07-11 15:39:20 -0400

    Fix awair script bug

--- device_code/awair.sh
+++ device_code/awair.sh
@@ -4,5 +4,5 @@ set -e
 
 AWAIR_URL=http://192.168.0.211/air-data/latest
 
-curl -s $AWAIR_URL | jq -j '"http://weather.rainskit.com/submit?source=awair&temperature=\(.temp)&humidity=\(.humid)&co2=\(.co2)&voc=\(.co2)&pm25=\(.pm25)"' | xargs curl -s -S > /dev/null
+curl -s $AWAIR_URL | jq -j '"http://weather.rainskit.com/submit?source=awair&temperature=\(.temp)&humidity=\(.humid)&co2=\(.co2)&voc=\(.voc)&pm25=\(.pm25)"' | xargs curl -s -S > /dev/null
 
