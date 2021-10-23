3dcc6e891004dc015a65acf83516db6e82f5392c
Nathan Arthur <narthur-git@rainskit.com>
2021-10-23 15:56:36 -0400

    Update awair.sh to use a hostname

--- device_code/awair.sh
+++ device_code/awair.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-AWAIR_URL=http://192.168.0.211/air-data/latest
+AWAIR_URL=http://awair.rainskit.com/air-data/latest
 
 curl -s $AWAIR_URL | jq -j '"http://weather.rainskit.com/submit?source=awair&temperature=\(.temp)&humidity=\(.humid)&co2=\(.co2)&voc=\(.voc)&pm25=\(.pm25)"' | xargs curl -s -S > /dev/null
 
