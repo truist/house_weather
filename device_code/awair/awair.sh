16f80b472023baea62278345be3aaae1c2179a22
Nathan Arthur <nathan@paceap.com>
2022-11-06 21:44:04 -0500

    Refactor awair.sh

--- device_code/awair/awair.sh
+++ device_code/awair/awair.sh
@@ -12,6 +12,8 @@ fi
 
 AWAIR_URL="https://developer-apis.awair.is/v1/users/self/devices/$DEVICE_TYPE/$DEVICE_ID/air-data/latest"
 
-curl -s "$AWAIR_URL" --header "Authorization: Bearer $API_KEY" | jq '.data[0].sensors | map({ key: (.comp), value: (.value)}) | from_entries' | jq -j '"http://weather.rainskit.com/submit?source=awair&temperature=\(.temp)&humidity=\(.humid)&co2=\(.co2)&voc=\(.voc)&pm25=\(.pm25)"' | xargs curl -s -S > /dev/null
-
+DATA="$(curl -s "$AWAIR_URL" --header "Authorization: Bearer $API_KEY")"
+FORMATTED="$(echo "$DATA" | jq '.data[0].sensors | map({ key: (.comp), value: (.value)}) | from_entries')"
+REQ_URL="$(echo "$FORMATTED" | jq -j '"http://weather.rainskit.com/submit?source=awair&temperature=\(.temp)&humidity=\(.humid)&co2=\(.co2)&voc=\(.voc)&pm25=\(.pm25)"')"
+curl -s -S "$REQ_URL" > /dev/null
 
