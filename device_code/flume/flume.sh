3eb3092d375db4b5bc302f78cab8224f95aee053
Nathan Arthur <narthur-git@rainskit.com>
2022-12-14 22:35:19 -0500

    Stupid Flume expires their access tokens regularly, no matter what

--- device_code/flume/flume.sh
+++ device_code/flume/flume.sh
@@ -1,36 +1,67 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-set -e
+set -eu
 
 CURDIR="$(dirname "$0")"
 . "$CURDIR/secrets.sh"
 
-if [ -z "$BEARER_TOKEN" ]; then
-       echo "You must set BEARER_TOKEN, USER_ID, and DEVICE_ID in secrets.sh" >&2
+if [ -z "$PASSWORD" ]; then
+       echo "You must set CLIENT_ID, CLIENT_SECRET, USERNAME, PASSWORD, USER_ID, and DEVICE_ID in secrets.sh" >&2
        exit 1
 fi
 
-QUERY_DATA_PREFIX='{ "queries": [ {"request_id": "request_1", "bucket": "MIN", "since_datetime": "'
-SINCE="$(date -d '-3 minutes' '+%Y-%m-%d %H:%M:%S')"
-QUERY_DATA_SUFFIX='", "operation": "SUM", "units": "GALLONS"}]}'
+# just use the password to get a wholly new access token every time,
+# because that's waaay easier than trying to track an ever-moving
+# refresh token, with forcibly-expiring access tokens
+read -rd '' QUERY_DATA <<EOF || true
+{
+    "grant_type": "password",
+    "client_id": "$CLIENT_ID",
+    "client_secret": "$CLIENT_SECRET",
+    "username": "$USERNAME",
+    "password": "$PASSWORD"
+}
+EOF
+# echo "$QUERY_DATA" | jq .
 
-QUERY_DATA="${QUERY_DATA_PREFIX}${SINCE}${QUERY_DATA_SUFFIX}"
+RESPONSE="$(curl -s \
+	--request POST \
+	--url https://api.flumewater.com/oauth/token \
+	--header 'accept: application/json' \
+	--header 'content-type: application/json' \
+	--data "$QUERY_DATA")"
+# echo "$RESPONSE" | jq .
+BEARER_TOKEN="$(echo "$RESPONSE" | jq -e -r .data[0].access_token)"
+# echo "BEARER_TOKEN: $BEARER_TOKEN"
 
-DATA="$(curl -s \
-  --request POST \
-  --url "https://api.flumetech.com/users/$USER_ID/devices/$DEVICE_ID/query" \
-  --header 'content-type: application/json' \
-  --header "Authorization: Bearer $BEARER_TOKEN" \
-  --data "$QUERY_DATA")"
 
-echo "$DATA"
+read -rd '' QUERY_DATA <<EOF || true
+{
+	"queries": [
+		{
+			"request_id": "request_1",
+			"bucket": "MIN",
+			"since_datetime": "$(date -d '-3 minutes' '+%Y-%m-%d %H:%M:%S')",
+			"operation": "SUM",
+			"units": "GALLONS"
+		}
+	]
+}
+EOF
+# echo "$QUERY_DATA" | jq .
 
-VOLUME="$(echo "$DATA" | jq -e '.data[0].request_1[0].value')"
+RESPONSE="$(curl -s \
+	--request POST \
+	--url "https://api.flumetech.com/users/$USER_ID/devices/$DEVICE_ID/query" \
+	--header 'content-type: application/json' \
+	--header "Authorization: Bearer $BEARER_TOKEN" \
+	--data "$QUERY_DATA")"
+echo "$RESPONSE"
 
+
+VOLUME="$(echo "$RESPONSE" | jq -e -r '.data[0].request_1[0].value')"
 REQ_URL="http://weather.rainskit.com/water?source=flume&volume=$VOLUME"
-#echo "$REQ_URL"
-
+# echo "REQ_URL: $REQ_URL"
 curl -s -S "$REQ_URL" > /dev/null
 
-exit 0
 
